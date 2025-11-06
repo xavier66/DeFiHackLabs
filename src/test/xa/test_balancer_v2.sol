5628723ec45a0878332b7e816c6ef07cfe85b67e
xavier <chenxueyan66@gmail.com>
2025-11-07 00:58:38 +0800

    change swap rate

--- src/test/xa/test_balancer_v2.sol
+++ src/test/xa/test_balancer_v2.sol
@@ -97,12 +97,12 @@ contract BalancerV2BatchSwapReplayTest is BaseTestWithBalanceLog {
 
     function _buildSwaps(uint256 bal_rETH0, uint256 bal_WETH0) internal pure returns (IBalancerVault.BatchSwapStep[] memory) {
 
-        IBalancerVault.BatchSwapStep[] memory swaps = new IBalancerVault.BatchSwapStep[](65);
+        IBalancerVault.BatchSwapStep[] memory swaps = new IBalancerVault.BatchSwapStep[](2);
 
         uint256 bal_rETH = bal_rETH0;
         uint256 i = 0 ;
-        uint256 rate = 50;
-        while(i < 65){
+        uint256 rate = 99;
+        while(i < 2){
             swaps[i] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_rETH * rate / 100, "");
             i = i + 1;
             bal_rETH -= bal_rETH * rate / 100;
