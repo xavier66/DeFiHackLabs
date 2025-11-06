592061a0b6f03286ba93e06790f4d2e5f63d08e2
xavier <chenxueyan66@gmail.com>
2025-11-07 00:21:31 +0800

    fix bug

--- src/test/xa/test_balancer_v2.sol
+++ src/test/xa/test_balancer_v2.sol
@@ -104,7 +104,7 @@ contract BalancerV2BatchSwapReplayTest is BaseTestWithBalanceLog {
         uint256 bal_rETH = bal_rETH0;
         uint256 i = 0 ;
         while(i < 10){
-            bal_rETH = bal_rETH * 99 / 100;
+            bal_rETH = bal_rETH * 50 / 100;
             swaps[i] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_rETH, "");
             i = i + 1;
         }
