d947f208475ae2023b0cfe5287845560d466eed2
xavier <chenxueyan66@gmail.com>
2025-11-07 00:15:16 +0800

    add info

--- src/test/xa/test_balancer_v2.sol
+++ src/test/xa/test_balancer_v2.sol
@@ -35,7 +35,7 @@ interface IBalancerVault {
     external
     view
     returns (
-        IERC20[] memory tokens,
+        address[] memory tokens,
         uint256[] memory balances,
         uint256 lastChangeBlock
     );
