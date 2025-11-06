6ea99126be4aeaac4ca76ad4a5aeabcdc467e0f9
xavier <chenxueyan66@gmail.com>
2025-11-07 03:15:58 +0800

    change swap rate

--- src/test/xa/test_balancer_v2.sol
+++ src/test/xa/test_balancer_v2.sol
@@ -105,7 +105,7 @@ contract BalancerV2BatchSwapReplayTest is BaseTestWithBalanceLog {
         // 目标是让它降低到 6 以内
         while(i < 20){
             uint256 amountOut = bal_rETH * rate / 100 ;
-            if (i == 3){
+            if (i == 4){
                 amountOut = amountOut / 3;
             }
             swaps[i] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, amountOut, "");
