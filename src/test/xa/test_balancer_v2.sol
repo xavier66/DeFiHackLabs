70222f27204542b089c0b0b01f11b7f964272bab
xavier <chenxueyan66@gmail.com>
2025-11-07 09:32:17 +0800

    change swap rate

--- src/test/xa/test_balancer_v2.sol
+++ src/test/xa/test_balancer_v2.sol
@@ -113,10 +113,10 @@ contract BalancerV2BatchSwapReplayTest is BaseTestWithBalanceLog {
 //            bal_rETH -= amountOut;
 //        }
         swaps[0] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_rETH -50, "");
-        swaps[1] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, 10, "");
-        swaps[2] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, 10, "");
-        swaps[3] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, 10, "");
-        swaps[4] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, 14, "");
+        swaps[1] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, 11, "");
+        swaps[2] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, 11, "");
+        swaps[3] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, 11, "");
+        swaps[4] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, 11, "");
 
 //        swaps[2] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_rETH * 50 / 100, "");
 //        swaps[3] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_rETH * 50 / 100, "");
