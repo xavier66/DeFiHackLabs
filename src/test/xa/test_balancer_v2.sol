6f2cb33c3e2a65b94d2f45070d961f16dbeccfae
xavier <chenxueyan66@gmail.com>
2025-11-07 09:25:17 +0800

    change swap rate

--- src/test/xa/test_balancer_v2.sol
+++ src/test/xa/test_balancer_v2.sol
@@ -112,7 +112,7 @@ contract BalancerV2BatchSwapReplayTest is BaseTestWithBalanceLog {
 //            i = i + 1;
 //            bal_rETH -= amountOut;
 //        }
-        swaps[0] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_rETH -6, "");
+        swaps[0] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_rETH -50, "");
 //        swaps[1] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_rETH * 50 / 100, "");
 //        swaps[2] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_rETH * 50 / 100, "");
 //        swaps[3] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_rETH * 50 / 100, "");
