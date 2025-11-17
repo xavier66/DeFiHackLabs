7f1392ce3f598df2d27040a8837fd13d745cb976
xavier <chenxueyan66@gmail.com>
2025-11-17 17:36:56 +0800

    test for cow balancer

--- src/test/xa/test_balancer_cow.sol
+++ src/test/xa/test_balancer_cow.sol
@@ -388,7 +388,6 @@ contract BalancerV2BatchSwapReplayTest is BaseTestWithBalanceLog {
     function testExploit() public balanceLog{
         uint256 numtoken = pool.getNumTokens();
         console.log("", numtoken);
-        pool.getCurrentTokens();
 
         console.log("tokens: ");
         uint256 bal0 = pool.getBalance(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0);
