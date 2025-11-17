faaee0f096773b6afd8a5162ee92c9a5ac6df92b
xavier <chenxueyan66@gmail.com>
2025-11-17 13:49:10 +0800

    change swap rate

--- src/test/xa/test_balancer_geclp.sol
+++ src/test/xa/test_balancer_geclp.sol
@@ -100,7 +100,7 @@ contract BalancerV2BatchSwapReplayTest is BaseTestWithBalanceLog {
 
         IBalancerVault.BatchSwapStep[] memory swaps = new IBalancerVault.BatchSwapStep[](2);
         swaps[0] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, bal_token0 * 116/100 + 20306683178563000000 - 100, "");
-        swaps[1] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, 20306683178563000000 * 10 , "");
+        swaps[1] = IBalancerVault.BatchSwapStep(POOL_ID, 1, 0, 203066831785630000000 , "");
 
         return swaps;
     }
